844b8fe2bedb0057f3b591870a704a1b59f6eabe
jwsadman <jawadsadman996@gmail.com>
2024-11-24 15:28:23 +0600

    Another commit

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ import numpy as np
 from ultralytics import YOLO
 from PIL import Image
 import os
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
 
-# Get the port from the environment variable, default to 8000
-PORT = int(os.getenv("PORT", 8000))
-
-Handler = SimpleHTTPRequestHandler
-
-with TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving on port {PORT}")
-    httpd.serve_forever()
 
 
 
